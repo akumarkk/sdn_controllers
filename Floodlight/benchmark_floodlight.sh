12f8110e3fd445291f0451cf5e1ee4ce554f150b
akumar0 <akumar0@node-1.akumar0-qv5259.cloudlab-pg0.apt.emulab.net>
2015-04-25 19:56:03 -0600

    updated

--- Floodlight/benchmark_floodlight.sh
+++ Floodlight/benchmark_floodlight.sh
@@ -1,15 +1,22 @@
 echo "Installing floodlight ....";
 
+mkdir tmp;
+
 ./install.sh
 
 echo "Setting up required bins ...";
+cp ./bin/* ./tmp/
+cd tmp;
+
+./install.sh
+
 cp stats.sh ./floodlight/;
 cp start.sh ./floodlight/;
-cp ./benchmark_throughput.sh ./floodlight/
+cp ./benchmark_throughput_macs.sh ./floodlight/
 
 cd ./floodlight
 pwd1=`pwd`
 echo "Entered [$pwd1/floodlight]"
 
-./benchmark_throughput.sh .
+./benchmark_throughput_macs.sh .
 
