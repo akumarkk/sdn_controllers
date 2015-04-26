21e6508bb96cb6449f146a49072eef388e433d17
akumarkk <akumarkk@outlook.com>
2015-04-25 19:26:21 -0600

    cleanup

--- cbench_installer.sh
+++
@@ -1,9 +0,0 @@
- sudo apt-get install autoconf automake libtool libsnmp-dev libpcap-dev
- sudo apt-get install pkg-config
- sudo apt-get install libconfig8-dev
- export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/libconfig.pc
- git clone git://gitosis.stanford.edu/openflow.git
- cd openflow; git checkout -b mybranch origin/release/1.0.0
- git clone git://gitosis.stanford.edu/oflops.git 
-cd oflops ; sh ./boot.sh ; ./configure --with-openflow-src-dir=<absolute path to openflow branch>; make
-cd oflops ; sudo make install
